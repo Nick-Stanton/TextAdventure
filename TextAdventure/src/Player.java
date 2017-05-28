e8af1514cee4ba0609b8a03fa89b1484e4cab5b4
jackSeery <Zebradog09@gmail.com>
2017-05-28 16:09:37 -0700

    lots of new stuff
    processCommand not implemented yet

--- TextAdventure/src/Player.java
+++ TextAdventure/src/Player.java
@@ -1,20 +1,52 @@
 import java.util.ArrayList;
 
-public class Player {
-
-	private ArrayList<String> inv;
+public class Player implements Character {
 	
-	public Player(){
-		inv = new ArrayList<String>();
+	private String name;
+	private int health; //depletes when attacked
+	private int magic; //depletes when used
+	private int power; //physical attack power
+	private ArrayList<String> items; //holds collected items
+	
+	public Player() {
+		health = 100;
+		magic = 100;
+		power = 10;
+		name = "player";
+		items = new ArrayList<String>();
 	}
 	
-	public String inventory(){
+	@Override
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public int getHealth() {
+		return health;
+	}
+	
+	@Override
+	public int getMagic() {
+		return magic;
+	}
+	
+	public int getPower() {
+		return power;
+	}
+	
+	public String getItems() {
 		String x = "";
-		for(String a: inv){
+		for(String a: items){
 			x += a;
 			x += ", ";
 		}
 		return x;
 	}
 	
+	@Override
+	public String processCommand(String command) { //will process command and allow player to interact with rooms/enemies
+		return null;
+	}
+
 }
