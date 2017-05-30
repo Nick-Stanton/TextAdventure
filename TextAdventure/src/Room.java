13df53d97924761f93cb30e48d75f1790c30ef73
StantonNick <nickstanton0214@gmail.com>
2017-05-30 09:00:02 -0700

    Update Room.java

--- TextAdventure/src/Room.java
+++ TextAdventure/src/Room.java
@@ -4,17 +4,29 @@ public class Room {
 
 	private boolean found;
 	private ArrayList<String> items;
+	private String description;
+	private String name;
 	
-	public Room(){
+	public Room(String name, String description){
 		found = false;
 		items = new ArrayList<String>();
+		this.description = description;
+		this.name = name;
 	}
 	
 	public boolean isFound(){
 		return found;
 	}
 	
+	public String getName(){
+		return name;
+	}
+	
 	public ArrayList<String> getItems(){
 		return items;
+	}
+	
+	public void look(){
+		System.out.println(name + "\n" + description);
 	}
 }
